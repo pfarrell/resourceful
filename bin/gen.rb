2a6a0b81da96dd9ce939c70bbfc9d17d0eed9f9d
Patrick Farrell <pat@cursor.com>
2017-10-26 14:39:56 -0700

    generic template processing

--- bin/gen.rb
+++ bin/gen.rb
@@ -1,27 +1,33 @@
 require 'byebug'
 require 'yaml'
 require 'erb'
+require 'active_support/inflector'
 require './lib/resourceful'
 
-@dest = "/home/pfarrell/proj/nimbus/src/main/kotlin/com/cursor/"
+@src_dest = "/home/pfarrell/proj/nimbus/src/main/kotlin/com/cursor/"
+@test_dest = "/home/pfarrell/proj/nimbus/src/test/kotlin/com/cursor/"
 
 def generate(model, template_path, out_dir)
- template = File.read(template_path)
- str = ERB.new(template).result(binding)
- File.write("#{@dest}#{out_dir}", str)
+  puts "processing: #{template_path}"
+  template = File.read(@templates + "/" + template_path)
+  str = ERB.new(template).result(binding)
+  File.write("#{out_dir}", str)
 end
 
 
 yml = YAML.load_file(ARGV[0])
 @model = Model.new(yml)
 
+@templates = "./lib/templates/#{ARGV[1]}/"
 
+Dir.entries(@templates).select{|x| x.end_with?("erb")}.each do |template|
+  basename = File.basename(template, ".erb")
+  next if basename =~ /migration/
+  camelcase = (basename == 'model') ? '' : basename.camelcase
+  camelcase = (basename == 'dao') ? 'DAO' : camelcase
+  dest = (basename =~ /_test/) ? @test_dest : @src_dest
+  generate(@model, template, "#{dest}#{basename.gsub(/_test/, '').pluralize}/#{@model.name}#{camelcase}.kt")
+end
 
-generate(@model, "./lib/templates/model.erb", "models/#{@model.name}.kt")
-generate(@model, "./lib/templates/dao.erb", "dao/#{@model.name}DAO.kt")
-generate(@model, "./lib/templates/mapper.erb", "mappers/#{@model.name}Mapper.kt")
-generate(@model, "./lib/templates/service.erb", "services/#{@model.name}Service.kt")
-generate(@model, "./lib/templates/resource.erb", "resources/#{@model.name}Resource.kt")
-
-template = File.read("./lib/templates/migration.erb")
+template = File.read("./lib/templates/#{ARGV[1]}/migration.erb")
 puts ERB.new(template).result(binding)
