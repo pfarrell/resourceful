d05a4dbd55e8949fb4091749a93b422649da8249
Patrick Farrell <pat@cursor.com>
2017-10-27 11:02:49 -0700

    add sample data support to fields

--- lib/resourceful/model.rb
+++ lib/resourceful/model.rb
@@ -1,37 +1,45 @@
 class Field
   attr_accessor :name, :codeType, :dbType
 
-	def initialize(name, hsh)
-		@name = name
-		@codeType = hsh['codeType']
-		@dbType = hsh['dbType']
-	end
+  def initialize(name, hsh)
+    @name = name
+    @codeType = hsh['codeType']
+    @dbType = hsh['dbType']
+  end
 
-	def snake_case
-		underscore(@name)
-	end
+  def snake_case
+    underscore(@name)
+  end
 
-	def underscore(camel_cased_word)
-			 camel_cased_word.to_s.gsub(/::/, '/').
-				 gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-				 gsub(/([a-z\d])([A-Z])/,'\1_\2').
-				 tr("-", "_").
-				 downcase
-	end
+  def underscore(camel_cased_word)
+       camel_cased_word.to_s.gsub(/::/, '/').
+         gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+         gsub(/([a-z\d])([A-Z])/,'\1_\2').
+         tr("-", "_").
+         downcase
+  end
+
+  def sample_data(i)
+    case @codeType
+    when "Long"
+      "#{i}L"
+    when "String"
+      "\"test_#{@name}\""
+    end
+  end
 end
 
 class Model
-	attr_accessor :yaml, :db_table, :package, :name, :model_fields, :link_fields
+  attr_accessor :yaml, :db_table, :package, :name, :model_fields, :link_fields
 
-	def initialize(hsh)
-		@name = hsh['name'].capitalize
+  def initialize(hsh) @name = hsh['name'].capitalize
     @db_table = hsh['dbTable']
-		@package = 'com.cursor'
-		@model_fields = hsh['modelFields'].map{|a| a.map{|k,v| Field.new(k, v)} }.flatten
-		@link_fields = hsh['linkFields'].map{|a| a.map{|k,v| Field.new(k, v)} }.flatten
-	end
+    @package = 'com.cursor'
+    @model_fields = hsh['modelFields'].map{|a| a.map{|k,v| Field.new(k, v)} }.flatten
+    @link_fields = hsh['linkFields'].map{|a| a.map{|k,v| Field.new(k, v)} }.flatten
+  end
 
-	def fields
-    @model_fields + @link_fields
+  def fields
+    @link_fields + @model_fields
   end
 end
